e0aad962fc80fdd97534252bf9f94c5cd0657630
Overheat-R <ramnikhilp@gmail.com>
2025-06-20 13:02:08 -0400

    CharacterBase

---
+++ Player/player.gd
@@ -0,0 +1,11 @@
+extends CharacterBody2D
+class_name Player
+
+var input_dir
+var SPEED: float = 30.0
+var ACCELERATION: float = 150.0
+var DECELERATION: float = 600.0
+
+func _physics_process(delta):
+	input_dir = Input.get_action_strength("right") - Input.get_action_strength("left")
+	
---
+++ Player/state_machine.gd
@@ -0,0 +1,22 @@
+extends Node
+
+@export var initial_state: PlayerState 
+@onready var current_state: PlayerState = initial_state
+
+var states: Dictionary = {}
+
+func _ready():
+	for child in get_children():
+		states[child.name] = child
+		child.Transition.connect(on_transition)
+		
+func _process(delta):
+	current_state.process_state(delta)
+	
+func _physics_process(delta):
+	current_state.physics_process_state(delta)
+	
+func on_transition(current_state: PlayerState, new_state: String):
+	current_state.exit()
+	current_state = states[new_state]
+	current_state.enter()
