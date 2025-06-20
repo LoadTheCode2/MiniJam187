3aef42c13aea59a7fe1cec274bccf20ca14e45bb
Joey Morin <joey.morin@outlook.com>
2025-06-20 15:14:37 -0400

    Refactor state transition event signatures in player states
    Updated the Transition.emit calls in idle.gd and run.gd to remove passing 'self' as an argument, now only emitting the new state name. Adjusted the on_transition function in state_machine.gd to match the new signature. Added a debug print statement in idle.gd for testing.

--- Player/idle.gd
+++ Player/idle.gd
@@ -2,6 +2,8 @@ extends PlayerState
 
 func process_state(delta):
 	if player.input_dir != Vector2.ZERO:
-		Transition.emit(self, "Run")
+		Transition.emit("Run")
+		print("I work?")
+		
 func physics_process_state(delta):
 	player.velocity = player.velocity.move_toward(Vector2.ZERO, player.DECELERATION * delta)
--- Player/state_machine.gd
+++ Player/state_machine.gd
@@ -1,6 +1,6 @@
 extends Node
 
-@export var initial_state: PlayerState 
+@export var initial_state: PlayerState
 @onready var current_state: PlayerState = initial_state
 
 var states: Dictionary = {}
@@ -17,7 +17,7 @@ func _physics_process(delta):
 	print(current_state)
 	current_state.physics_process_state(delta)
 	
-func on_transition(current_state: PlayerState, new_state: String):
+func on_transition(new_state: String):
 	current_state.exit()
 	current_state = states[new_state]
 	current_state.enter()
