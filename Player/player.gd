e9bde98d8f42438986133b58a3ead69d0c008e29
Overheat-R <ramnikhilp@gmail.com>
2025-06-21 16:21:41 -0400

    Update player animations and level background
    Expanded the player spritesheet and added a new 'Fall' animation. Updated the animation state machine to use a unified 'All' state and adjusted blend space points. Increased the player sprite's horizontal frames, updated the background size in level1, and set the main scene in project settings. Minor code change to match new animation tree parameter.

--- Player/player.gd
+++ Player/player.gd
@@ -11,7 +11,7 @@ var gravity: float = 980.0
 
 func _physics_process(delta):
 	input_dir = Vector2(Input.get_axis("left", "right"), 0.0)
-	anim_tree.set("parameters/R+I+J/blend_position", velocity)
+	anim_tree.set("parameters/All/blend_position", velocity)
 	
 	if not is_on_floor():
 		velocity.y += gravity * delta
