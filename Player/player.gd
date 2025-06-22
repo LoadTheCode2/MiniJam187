a596288219a721fbc90d7d4bcad172772eadf9a9
Joey Morin <joey.morin@outlook.com>
2025-06-22 10:10:20 -0400

    Replace JumpMeter with EnergyMeter and update player logic
    Removed the JumpMeter HUD and introduced a new EnergyMeter HUD with associated scripts, scenes, and assets. Updated player logic to use energy and jump strength from the new EnergyMeter, including energy consumption and regeneration. Also reorganized background assets and updated references in scenes to reflect these changes.

--- Player/player.gd
+++ Player/player.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 class_name Player
 @onready var anim_tree = %AnimationTree
+@onready var energy_meter = $GameUI/EnergyMeter
 
 var input_dir: Vector2
 var SPEED: float = 150.0
@@ -16,7 +17,11 @@ func _physics_process(delta):
 	
 	if not is_on_floor():
 		velocity.y += gravity * delta
-	elif Input.is_action_just_pressed("jump"):
-		velocity.y = BASE_JUMP + JUMP_MULTIPLIER * $GameUi/JumpMeter.jump_strength
+	elif Input.is_action_just_pressed("jump") and energy_meter.energy_remaining >= energy_meter.jump_strength:
+		velocity.y = BASE_JUMP + JUMP_MULTIPLIER * energy_meter.jump_strength
+		energy_meter.energy_remaining -= energy_meter.jump_strength
+		
+	if velocity != Vector2.ZERO:
+		energy_meter.lastEnergyRegen = 0
 	
 	move_and_slide()
